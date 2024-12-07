2492bf64286dcaca899d2592b337fa090dbf1a35
tlevesconte <tlevesconte@proton.me>
2024-12-07 09:57:21 +0000

    Refactor

--- 01/solution.go
+++ 01/solution.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"bufio"
-	"os"
 	"fmt"
+	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -36,4 +37,37 @@ func main() {
 
 	fmt.Println("01/P1:", solveP1(left, right))
 	fmt.Println("01/P2:", solveP2(left, right))
+}
+
+func solveP1(left, right []int) int {
+	slices.Sort(left)
+	slices.Sort(right)
+
+	var diff int
+	for i := range left {
+		diff += abs(left[i], right[i])
+	}
+
+	return diff
+}
+
+func abs(x, y int) int {
+	if x < y {
+		return y - x
+	}
+	return x - y
+}
+
+func solveP2(left, right []int) int {
+	simMap := make(map[int]int)
+	for _, val := range right {
+		simMap[val]++
+	}
+
+	var simScore int
+	for _, val := range left {
+		simScore += val * simMap[val]
+	}
+
+	return simScore
 }
